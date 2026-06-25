81bc7300b78f0655bdc57871c375e3b057e03c0f
Max Orlov <maks_orlov_orel@mail.ru>
2026-06-25 15:12:20 +0300

    feat: add endpoints for emulator lifecycle

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import uvicorn
 import time
 from fastapi import FastAPI
 
-
 app = FastAPI()
 
 
@@ -12,17 +11,28 @@ def get_emulators():
     try:
 
         result = subprocess.run(["adb", "devices"], capture_output=True, text=True, check=True)
-        words = result.stdout.split()
-
-        if len(words) > 4:
-            device_id = words[4]
-        else:
-            device_id = "Нет активных устройств"
+        device_ids = [line.split()[0] for line in result.stdout.splitlines()[1:] if line.strip()]
 
         return {
             "status": "success",
             "raw_output": result.stdout,
-            "device_id" : device_id
+            "device_ids": device_ids
+        }
+
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+
+
+@app.get("/emulators/available")
+def get_available_emulators():
+    try:
+
+        result = subprocess.run(["emulator", "-list-avds"], capture_output=True, text=True, check=True)
+        avds = [line for line in result.stdout.splitlines() if line.strip()]
+
+        return {
+            "status": "success",
+            "available_emulators": avds
         }
 
     except Exception as e:
@@ -32,11 +42,12 @@ def get_emulators():
 @app.post("/emulator/start")
 def start_emulator(emulator_name: str):
     try:
+
         subprocess.Popen(["emulator", "-avd", emulator_name])
 
         return {
             "status": "success",
-            "emulator" : f'Эмулятор {emulator_name} - запускается'
+            "emulator": f'Эмулятор {emulator_name} - запускается'
         }
 
     except Exception as e:
@@ -46,6 +57,7 @@ def start_emulator(emulator_name: str):
 @app.delete("/emulator/stop")
 def stop_emulator(device_id: str):
     try:
+
         subprocess.run(["adb", "-s", device_id, "emu", "kill"], check=True)
 
         return {
@@ -55,6 +67,38 @@ def stop_emulator(device_id: str):
 
     except Exception as e:
         return {"status": "error", "message": str(e)}
+
+
+@app.post("/app/install")
+def app_install(device_id: str, apk_path: str):
+    try:
+
+        result = subprocess.run(["adb", "-s", device_id, "install", apk_path], capture_output=True, text=True, check=True)
+
+        return {
+            "status": "success",
+            "message": f"Приложение с путем {apk_path}, установлено на устройство {device_id}",
+            "text": result.stdout
+        }
+
+    except Exception as e:
+       return {"status": "error", "message": str(e)}
+
+
+@app.post("/appium/start")
+def appium_start():
+    try:
+
+        subprocess.Popen(["appium"], shell = True)
+
+        return {
+            "status": "success",
+            "message": f"Appium сервер запущен",
+        }
+
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
